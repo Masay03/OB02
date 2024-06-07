18f9805deb041d85798a92a99e1333fd083702e3
Masay03 <maks.bz15@mail.ru>
2024-06-07 15:19:23 +0700

    новый код

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-class Bird():
-    def __init__(self, name, voice, color):
-        self.name = name
-        self.voice = voice
-        self.color = color
+class User():
+    def __init__(self, user_id, name ):
+        self._user_id = user_id
+        self._name = name
+        self._level = "user"
 
-    def fle(self):
-        print(f"{self.name} летает!")
+    def get_user_id(self):
+        return self._user_id
 
-    def eat(self):
-        print(f"{self.name} ест!")
+    def get_name(self):
+        return self._name
 
-    def sing(self):
-        print(f"{self.voice} поет!")
+    def get_level(self):
+        return self._level
 
-    def info(self):
-        print(f"Имя: {self.name}")
-        print(f"Цвет: {self.color}")
-        print(f"Голос: {self.voice}")
-
-class Pigeon(Bird):
-    def __init__(self, name, voice, color,favorite_food):
-        super().__init__(name, voice, color)
-        self.favorite_food = favorite_food
-
-    def walk(self):
-        print(f"{self.name} гуляет!")
-
-bird1 = Pigeon("Рыжик", "Кря-кря", "Желтый", "Семечки")
-
-bird2 = Bird("Кеша", "Кря-кря", "Красный")
-
-bird1.sing()
-bird1.info()
-bird1.walk()
+    def set_name(self, name):
+        self._name = name
 
 
+class Admin(User):
+    def __init__(self, user_id, name):
+        super().__init__(user_id, name)
+        self._level = "admin"
+
+    def add_user(self, user_list, user):
+        user_list.append(user)
+        print(f"Пользователь успешно добавлен в список: {user}.")
+
+    def remove_user(self, user_list, user):
+        user_list.remove(user)
+        print(f"Пользователь успешно удален из списка: {user}.")
+
+users = []
+admin = Admin(1, "admin")
+admin.add_user(users, User(2, "user1"))
+admin.add_user(users, User(3, "user2"))
+admin.remove_user(users, User(3, "user2"))
